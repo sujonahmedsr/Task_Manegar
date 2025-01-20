e983aa938276f2bb03277c2d4897014a93c8101f
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-01-21 00:43:48 +0600

    all okay

--- src/app/modules/authentications/authValidation.ts
+++ src/app/modules/authentications/authValidation.ts
@@ -6,6 +6,6 @@ export const userRegisterValidation = z.object({
     password: z.string({required_error: "Password must be provieded!"}),
 })
 export const userLoginValidation = z.object({
-    email: z.string({required_error: "Email must be provieded!"}),
+    email: z.string({required_error: "Email must be  provieded!"}),
     password: z.string({required_error: "Password must be provieded!"})
 })
