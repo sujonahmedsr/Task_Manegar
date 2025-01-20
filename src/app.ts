f7e22b58f216ff8a87ac63a05c968f16761e95d8
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-01-20 21:29:56 +0600

    updated

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express()
 
 // Middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({ origin: ['http://localhost:5000'], credentials: true }))
 app.use(cookieParser())
 
 
