c139831cc84f1cd2cfb89ac5c1add48fea43acb7
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-03-25 15:59:36 +0600

    okay

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express()
 
 // Middlewares
 app.use(express.json())
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173', 'https://dotask-gray.vercel.app/'], credentials: true }));
 app.use(cookieParser())
 
 
